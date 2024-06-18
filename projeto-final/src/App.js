c10cf2dd96e6afe3275555eda6810d2728ff236c
juvasconcelos1975 <joanavasconcelos99@gmail.com>
2024-06-18 12:19:17 +0100

    navbar almost done

--- projeto-final/src/App.js
+++ projeto-final/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './Navbar/Navbar';
+import Main from './Main/Main';
+import Gallery from './Gallery/Gallery';
+import About from './About/About';
+import Contact from './Contact/Contact';
+import Tempo from './Tempo/Tempo';
+import Footer from './Footer/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<Navbar />
+				<Routes>
+					<Route path='/' exact element={<Main />} />
+					<Route path='/about' element={<About />} />
+					
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ projeto-final/src/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+
+	return (
+		<nav className='navbar'>
+			<div className='navbar-container'>
+				<div className='navbar-logo'>
+					<a href='/'>JewelryBrand</a>
+				</div>
+				<div className={`navbar-menu ${isOpen ? 'active' : ''}`}>
+					<a href='/' className='navbar-link'>
+						Home
+					</a>
+					<a href='/shop' className='navbar-link'>
+						Shop
+					</a>
+					<a href='/about' className='navbar-link'>
+						About
+					</a>
+					<a href='/contact' className='navbar-link'>
+						Contact
+					</a>
+				</div>
+				<div className='navbar-toggle' onClick={toggleMenu}>
+					<span className='navbar-toggle-icon'>&#9776;</span>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
